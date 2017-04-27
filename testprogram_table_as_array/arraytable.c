a2f5a3fce851cc118875e70e8230038eb480976e
Bjnygren_laptop <bjoeern_n87@hotmail.com>
2017-04-27 19:21:04 +0200

    workong - no comments

--- testprogram_table_as_array/arraytable.c
+++ testprogram_table_as_array/arraytable.c
@@ -15,11 +15,24 @@
 
 typedef struct ArrayTable{
 	array *values;
+	array *keys;
 	CompareFunction *cf;
 	KeyFreeFunc *keyFree;
 	ValueFreeFunc *valueFree;
 	int nrOccupied;
 } ArrayTable;
+
+/*
+ * 
+ * 
+ */
+ void table_setValue(Table *table, KEY key, VALUE value, int index){
+	ArrayTable *a = (ArrayTable*)table;
+	array_setMemHandler(a->keys, a->keyFree); 			
+	array_setValue(a->keys,key,index,0);
+	array_setMemHandler(a->values, a->valueFree);			
+	array_setValue(a->values,value,index,1);
+ }
 
 /* Creates a table.
  *  compare_function - Pointer to a function that is called for comparing
@@ -32,10 +45,14 @@ Table *table_create(CompareFunction *compare_function){
 	ArrayTable *a = calloc(sizeof (ArrayTable),1); //varför argument i den ordningen? vad säger vi här? calloc->0 så 0 == empty, n+1==returned?
 //	if(!a);	//kollar så att a fick allokerat minne, annars skapas inte a??
 //		return NULL;
-	a->values = array_create(2,0,0,NUM_ELEMENTS,NUM_ELEMENTS); //col 1=index, col2=key, col3=value
+	a->values = array_create(1,0,NUM_ELEMENTS); //col 1=index, col2=key, col3=value
+	a->keys = array_create(1,0,NUM_ELEMENTS);
 	a->cf = compare_function;
-	if(array_hasValue(a->values,0,0) == 0){
+	if((array_hasValue(a->keys,0) == 0) && (array_hasValue(a->values,0) == 0)){
 		a->nrOccupied = 0;
+	}
+	else {
+		return NULL;
 	}
 	
 	return a;
@@ -48,7 +65,6 @@ Table *table_create(CompareFunction *compare_function){
 void table_setKeyMemHandler(Table *table,KeyFreeFunc *freeFunc){
 	ArrayTable *a = (ArrayTable*)table;
 	a->keyFree = freeFunc;
-	array_setMemHandler(a->values,a->keyFree);
 }
 
 /* Install a memory handling function responsible for removing a value when removed from the table
@@ -65,7 +81,7 @@ void table_setValueMemHandler(Table *table,ValueFreeFunc *freeFunc){
  * Returns: false if the table is not empty, true if it is. */
 bool table_isEmpty(Table *table){
 	ArrayTable *a = (ArrayTable*)table;
-	return array_hasValue(a->values,0,1) == 0;
+	return (array_hasValue(a->keys,0) == 0);
 }
 
 /* Inserts a key and value pair into the table. The table type takes
@@ -80,11 +96,10 @@ void table_insert(Table *table, KEY key, VALUE value){
 	KEY key2;
 	int key_set = 0;
 	int i = 0;
-	while(array_hasValue(a->values,i,0)){ // kolla om det finns dubletter
-		key2 = array_inspectValue(a->values,i,0);
+	while(array_hasValue(a->values,i)){ // kolla om det finns dubletter
+		key2 = array_inspectValue(a->keys,i);
 		if(a->cf(key,key2) == 0){
-			array_setValue(a->values,key,i,0);
-			array_setValue(a->values,value,i,1);
+			table_setValue(a, key, value, i);
 			key_set = 1;
 			break;
 		}
@@ -95,12 +110,13 @@ void table_insert(Table *table, KEY key, VALUE value){
 	}
 	if(key_set == 0){ // fylla på längst bak eller avbryta
 		if(i<=NUM_ELEMENTS-1){
-			array_setValue(a->values,key,i,0);
-			array_setValue(a->values,value,i,1);
+			table_setValue(a, key, value, i);
 			a->nrOccupied++;
 		}
 	}
 }
+
+
 
 
 /* Finds a value given its key.
@@ -115,20 +131,21 @@ VALUE table_lookup(Table *table, KEY key){
 	ArrayTable *a = (ArrayTable*)table;
 	KEY key2;
 	int i = 0;
-	while(array_hasValue(a->values,i,0)){ // söker nyckel
-		key2 = array_inspectValue(a->values,i,0);
+	while(array_hasValue(a->keys,i)){ // söker nyckel
+		key2 = array_inspectValue(a->keys,i);
 		if(a->cf(key,key2) == 0){
 			break;
 		}
 		i++;
 		if(i>NUM_ELEMENTS-1){
-			break;
+			return NULL;
 		}
 	}
 	return array_inspectValue(a->values,i,1);
 }
 
-/* Removes an item from the table given its key.
+/* Removes an item from the table given its key. Goes through
+ *  every item, thus all duplicates are removed. (Unnceccesary?)
  *  table - Pointer to the table.
  *  key   - Pointer to the item's key.
  */
@@ -139,29 +156,27 @@ void table_remove(Table *table, KEY key){
 	const int memsize = sizeof key2; // för att komma runt att memcpy inte gillar (key2, XXX, sizeof(key2)) som arg med -Wall
 	int i = 0;
 	int j = 0;
-	while(array_hasValue(a->values,i,1)){ // söker nyckel
-		key2 = array_inspectValue(a->values,i,0);
+	while(array_hasValue(a->values,i)){ // söker nyckel
+		key2 = array_inspectValue(a->keys,i);
 		if(a->cf(key,key2) == 0){
-			while(array_hasValue(a->values,j+1,0)){
+			while(array_hasValue(a->values,j+1)){ //moves every item behind 'key' one step forward, removes 'key'
 				
-				key2 = array_inspectValue(a->values,j+1,0);
-				value2 = array_inspectValue(a->values,j+1,1);
+				key2 = array_inspectValue(a->keys,j+1);
+				value2 = array_inspectValue(a->values,j+1);
 				KEY key_move = malloc(sizeof key_move);
 				VALUE value_move = malloc(sizeof value_move);
 				memcpy(key_move, key2, memsize);
 				memcpy(value_move, value2, memsize);
-				array_setValue(a->values,key_move,j,0);
-				array_setValue(a->values,value_move,j,1);
+				
+				table_setValue(a, key_move, value_move, j);
 				j++;
 			}
-			array_setValue(a->values,NULL,j,0);
-			array_setValue(a->values,NULL,j,1);
+			table_setValue(a, NULL, NULL, j);
 			break;
 		}
 		j++;
 		i++;
 		if(i>NUM_ELEMENTS-1){
-//			printf("No such element to remove.");
 			break;
 		}
 	}
@@ -170,6 +185,13 @@ void table_remove(Table *table, KEY key){
 
 void table_free(Table *table){
 	ArrayTable *a = (ArrayTable*)table;
+	if(a->valueFree != NULL){
+	array_setMemHandler(a->values,a->keyFree);
 	array_free(a->values);
-	free(table);
+	}
+	if(a->keyFree != NULL){
+	array_setMemHandler(a->keys,a->keyFree);
+	array_free(a->keys);
+	}
+	free(a);
 }
